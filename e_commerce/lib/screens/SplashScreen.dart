32f7395244fbd151589223ed53b548724dc93f87
EngAhmed2 <ma6628758@gmail.com>
2024-02-22 17:29:45 +0200

    Finish App icon and Splash Screen

---
+++ e_commerce/lib/screens/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'package:e_commerce/screens/MainScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3),(){
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>const MainScreen()), (route) => false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF40BFFF),
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.light,
+        ),
+      ),
+      backgroundColor: const Color(0xFF40BFFF),
+      body: Center(
+        child: Image.asset('assets/images/white_logo.png'),
+      ),
+    );
+  }
+}
